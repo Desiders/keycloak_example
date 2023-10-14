484b8f01708f46490bf9d87f76cb7b525ee8de1d
Desiders <abc_desi@proton.me>
2023-10-14 12:40:03 +0300

    Add using `OAuth2AuthorizationCodeBearer` for swagger

--- src/handlers/users.py
+++ src/handlers/users.py
@@ -1,8 +1,8 @@
 import logging
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Request
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2AuthorizationCodeBearer
 
 from src.providers import Stub
 from src.providers.keycloak.client import KeycloakClient, OIDCUser
@@ -15,26 +15,23 @@ user_router = APIRouter(
 )
 
 
-@user_router.get("/")
+@user_router.get("/", response_model=OIDCUser)
 async def get_user(
-    request: Request,
     keycloak: Annotated[KeycloakClient, Depends(Stub(KeycloakClient))],
+    access_token: Annotated[
+        str,
+        Depends(
+            OAuth2AuthorizationCodeBearer(
+                authorizationUrl="/auth/login",
+                tokenUrl="/auth/token",
+            )
+        ),
+    ],
 ) -> OIDCUser:
-    logger.info("Getting user")
+    logger.info("Getting user", extra={"access_token": access_token})
 
-    openid_configuration = await keycloak.get_openid_configuration()
+    user = await keycloak.get_user_by_token(access_token)
 
-    token_endpoint = openid_configuration.token_endpoint
-
-    user_access_token = await OAuth2PasswordBearer(tokenUrl=token_endpoint)(request)
-
-    if user_access_token is None:
-        logger.debug("No access token provided")
-
-        raise Exception("No access token provided")
-
-    user = await keycloak.get_user_by_token(user_access_token)
-
-    logger.debug(f"User: {user}")
+    logger.debug("Got user", extra={"user": user})
 
     return user
