106ec297d3886531eb6ceff8897607e3c671b977
Desiders <abc_desi@proton.me>
2023-10-14 12:46:11 +0300

    Remove constants

--- src/handlers/exceptions.py
+++ src/handlers/exceptions.py
@@ -5,13 +5,11 @@ from fastapi.responses import RedirectResponse
 
 logger = logging.getLogger(__name__)
 
-AUTH_LOGIN_URL = "/auth/login"
-
 
 async def unauthorized_handler(request: Request, exc: HTTPException):
     logger.debug(
         "Unauthorized exception. Redirecting to login URL",
-        extra={"exc": exc, "login_url": AUTH_LOGIN_URL},
+        extra={"exc": exc, "login_url": "/auth/login"},
     )
 
-    return RedirectResponse(AUTH_LOGIN_URL)
+    return RedirectResponse("/auth/login")
